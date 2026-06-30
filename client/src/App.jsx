7eb9b2001925eb888f2a45d4bcd84eae6dfd169a
Logesh pr <118522095+Logesh-pr@users.noreply.github.com>
2026-06-30 21:27:33 +0530

    fix favicon

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -48,6 +48,7 @@ const router = createBrowserRouter([
             path: "/",
             element: <Home />,
           },
+
           {
             path: "/search",
             element: <SearchPage />,
